8c6dcbcca3e75ab718f1c2382792eb06e2cb2c1c
Malin Jakob <malin.jakob98@gmail.com>
2024-10-24 22:32:03 +0200

    add changes too image gallery

--- 3.2.1/js/script.js
+++ 3.2.1/js/script.js
@@ -4,33 +4,31 @@ function preloadImages () {
     const img2 = new Image()
     const img3 = new Image()
 
-    img1.src = '/images/img1.jpg'
-    img2.src = '/images/img2.jpg'
-    img3.src = '/images/img3.jpg'
+    img1.src = 'images/img1.jpg'
+    img2.src = 'images/img2.jpg'
+    img3.src = 'images/img3.jpg'
   }
 }
 
-const images = ['/images/img1.jpg', '/images/img2.jpg', '/images/img3.jpg']
-let changeImageTimer;
-let imgIndex = 0 
+const images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg']
+let imgIndex = 0
 
-function changeImage(){
-  currentImage = (currentImage + 1) % images.length
-  $('#currentImage').attr('src' , images[imgIndex])
+function changeImage () {
+  imgIndex++
+  let currentImage
+  currentImage = (imgIndex) % images.length
+  $('#currentImage').attr('src', images[currentImage])
 }
 
-function imageTimer() {
-  changeImageTimer = setInterval(changeImage, 2000);
+function imageTimer () {
+  let changeImageTimer
+  changeImageTimer = setInterval(changeImage, 2000)
 }
 
-$(function(){
-  let $img = $('#currentImg')
-  $img.on('mouseover', function(){
+$(document).ready(function () {
+  preloadImages()
+  imageTimer()
+  $('#currentImage').on('mouseover', function () {
     changeImage()
   })
 })
-
-preloadImages()
-imageTimer()
-
-
